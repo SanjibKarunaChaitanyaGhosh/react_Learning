453a98cb641b1b14602b8d025916bb1d40bfcf34
SanjibKarunaChaitanyaGhosh <sanjibghosh791@gmail.com>
2026-07-16 09:57:47 +0530

    advanced useStates

--- phase-10(useState Advanced)/src/App.jsx
+++ phase-10(useState Advanced)/src/App.jsx
@@ -3,6 +3,7 @@ import React, { useState } from 'react'
 const App = () => {
 
   const [num, setNum] = useState({user:"Sanjib",age:20})
+  const [num1, setNum1] = useState({user:"Arnab",age:20})
 
   // DESTRUCTURING METHOD TO CHANGE THE USER AND AGE
   const btnClicked=()=>{
@@ -10,8 +11,9 @@ const App = () => {
     const newNum={...num};
     newNum.user="Ghosh"
     setNum(newNum)
-
-    setNum(prev=>({...prev,age:50}))
+    
+    setNum1(prev=>({...prev,age:50}))
+    setNum1(prev=>({...prev,age:50}))
   }
   return (
     <div>
